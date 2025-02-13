ce94cb27fb7fbd1c7a73d89ef322f69b41166d83
santan maddi <saisantan2004@gmail.com>
2025-02-13 11:50:14 +0800

    fix(schemas): minor fixes within before_all sql script

--- packages/schemas/tables/_before_all.sql
+++ packages/schemas/tables/_before_all.sql
@@ -1,6 +1,7 @@
 /* This SQL will run before all other queries. */
-
+create schema logto if not exists;
+set search_path = logto, "$user";
 create role logto_tenant_${database} password '${password}' noinherit;
 
 GRANT USAGE ON SCHEMA logto TO logto_tenant_${database};
-ALTER ROLE logto_tenant_${database} SET search_path = logto, logto, "$user";
+ALTER ROLE logto_tenant_${database} SET search_path = logto, "$user";
